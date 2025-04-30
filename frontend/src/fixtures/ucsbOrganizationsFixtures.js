daf86ac4e634a95d8db2674a1c1aa11e6d48090a
Roy Ren <royren@169-231-60-118.wireless.ucsb.edu>
2025-04-29 17:58:11 -0700

    add UCSB Org fixtures

---
+++ frontend/src/fixtures/ucsbOrganizationsFixtures.js
@@ -0,0 +1,35 @@
+const UCSBOrganizationFixtures = {
+  oneUCSBOrganization: [
+    {
+      orgCode: "ZPY",
+      orgTranslationShort: "ZETA PHI RHO",
+      orgTranslation: "ZETA PHI RHO",
+      inactive: false,
+    },
+  ],
+
+  threeUCSBOrganization: [
+    {
+      orgCode: "SKY",
+      orgTranslationShort: "SKYDIVING CLUB",
+      orgTranslation: "SKYDIVING CLUB AT UCSB",
+      inactive: false,
+    },
+
+    {
+      orgCode: "OSLI",
+      orgTranslationShort: "STUDENT LIFE",
+      orgTranslation: "OFFICE OF STUDENT LIFE",
+      inactive: false,
+    },
+
+    {
+      orgCode: "KRC",
+      orgTranslationShort: "KOREAN RADIO CL",
+      orgTranslation: "KOREAN RADIO CLUB",
+      inactive: false,
+    },
+  ],
+};
+
+export { UCSBOrganizationFixtures };
