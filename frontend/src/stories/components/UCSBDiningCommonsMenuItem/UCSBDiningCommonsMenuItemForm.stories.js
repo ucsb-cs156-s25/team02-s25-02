2f27c82a305141b03a3316c6e895b3e73496c99e
Xinyan Zhao <xinyan_zhao@ucsb.edu>
2025-05-01 03:24:40 -0700

    xz - create form component

---
+++ frontend/src/stories/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm.stories.js
@@ -0,0 +1,28 @@
+import React from "react";
+import UCSBDiningCommonsMenuItemForm from "main/components/UCSBDiningCommonsMenuItem/UCSBDiningCommonsMenuItemForm";
+
+export default {
+  title: "Forms/UCSBDiningCommonsMenuItemForm",
+  component: UCSBDiningCommonsMenuItemForm,
+};
+
+const Template = (args) => <UCSBDiningCommonsMenuItemForm {...args} />;
+
+export const Create = Template.bind({});
+Create.args = {
+  initialContents: {},
+  submitAction: (data) => console.log("Create form submitted", data),
+  buttonLabel: "Create",
+};
+
+export const Update = Template.bind({});
+Update.args = {
+  initialContents: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Tacos",
+    station: "Main Station",
+  },
+  submitAction: (data) => console.log("Update form submitted", data),
+  buttonLabel: "Update",
+};
