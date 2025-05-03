f6832501fe619e068f8b4cbd73e7d1ac79146556
Evania Cheng <evania@ucsb.edu>
2025-05-02 19:01:30 -0700

    ec-format

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -28,7 +28,6 @@ import { hasRole, useCurrentUser } from "main/utils/currentUser";
 import "bootstrap/dist/css/bootstrap.css";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function App() {
   const { data: currentUser } = useCurrentUser();
 
