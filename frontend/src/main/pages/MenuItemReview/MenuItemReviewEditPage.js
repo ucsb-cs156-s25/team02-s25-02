8927ae342d655bba05cd6ea5a7202b96bc0e0e26
Andrew Wang <andrewwang56@foxmail.com>
2025-05-06 22:45:34 -0700

    Implement Menu Item Review Create, Edit, and Index pages with form handling and navigation.

--- frontend/src/main/pages/MenuItemReview/MenuItemReviewEditPage.js
+++ frontend/src/main/pages/MenuItemReview/MenuItemReviewEditPage.js
@@ -1,11 +1,79 @@
 import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+import { useParams } from "react-router-dom";
+import MenuItemReviewForm from "main/components/MenuItemReview/MenuItemReviewForm";
+import { Navigate } from "react-router-dom";
+import { useBackend, useBackendMutation } from "main/utils/useBackend";
+import { toast } from "react-toastify";
 
-export default function MenuItemReviewEditPage() {
-  // Stryker disable all : placeholder for future implementation
+export default function MenuItemReviewEditPage({ storybook = false }) {
+  let { id } = useParams();
+
+  const {
+    data: menuitemreview,
+    _error,
+    _status,
+  } = useBackend(
+    // Stryker disable next-line all : don't test internal caching of React Query
+    [`/api/menuitemreviews?id=${id}`],
+    {
+      // Stryker disable next-line all : GET is the default, so mutating this to "" doesn't introduce a bug
+      method: "GET",
+      url: `/api/menuitemreview`,
+      params: {
+        id,
+      },
+    },
+  );
+
+  const objectToAxiosPutParams = (menuitemreview) => ({
+    url: "/api/menuitemreview",
+    method: "PUT",
+    params: {
+      id: menuitemreview.id,
+    },
+    data: {
+      itemId: menuitemreview.itemId,
+      reviewerEmail: menuitemreview.reviewerEmail,
+      stars: menuitemreview.stars,
+      dateReviewed: menuitemreview.dateReviewed,
+      comments: menuitemreview.comments,
+    },
+  });
+
+  const onSuccess = (menuitemreview) => {
+    toast(
+      `MenuItem Review Updated - id: ${menuitemreview.id} itemId: ${menuitemreview.itemId} reviewerEmail: ${menuitemreview.reviewerEmail} stars: ${menuitemreview.stars} dateReviewed: ${menuitemreview.dateReviewed} comments: ${menuitemreview.comments}`,
+    );
+  };
+
+  const mutation = useBackendMutation(
+    objectToAxiosPutParams,
+    { onSuccess },
+    // Stryker disable next-line all : hard to set up test for caching
+    [`/api/menuitemreview?id=${id}`],
+  );
+
+  const { isSuccess } = mutation;
+
+  const onSubmit = async (data) => {
+    mutation.mutate(data);
+  };
+
+  if (isSuccess && !storybook) {
+    return <Navigate to="/menuitemreview" />;
+  }
+
   return (
     <BasicLayout>
       <div className="pt-2">
-        <h1>Edit page not yet implemented</h1>
+        <h1>Edit Restaurant</h1>
+        {menuitemreview && (
+          <MenuItemReviewForm
+            submitAction={onSubmit}
+            buttonLabel={"Update"}
+            initialContents={menuitemreview}
+          />
+        )}
       </div>
     </BasicLayout>
   );
